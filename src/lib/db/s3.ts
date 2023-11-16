f79cddbed790bdf9c9827388e8e5cbb899124eed
Kimmi17 <dangthikimthuybl@gmail.com>
2023-11-16 17:08:39 +0200

    Add file upload to Amazon S3 and S3 URL retrieval.

--- src/lib/db/s3.ts
+++ src/lib/db/s3.ts
@@ -13,5 +13,37 @@ export async function uploadToS3(file: File) {
       },
       region: "eu-north-1",
     });
+
+    const file_key =
+      "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
+    const params = {
+      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+      Key: file_key,
+      Body: file,
+    };
+
+    const upload = s3
+      .putObject(params)
+      .on("httpUploadProgress", (evt) => {
+        console.log(
+          "uploading to s3...",
+          parseInt(((evt.loaded * 100) / evt.total).toString())
+        ) + "%";
+      })
+      .promise();
+
+    await upload.then((data) => {
+      console.log("successfully uploaded to S3!", file_key);
+    });
+
+    return Promise.resolve({
+      file_key,
+      file_name: file.name,
+    });
   } catch (error) {}
+}
+
+export function getS3Url(file_key: string) {
+  const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.eu-north-1.amazonaws.com/${file_key}`;
+  return url;
 }
