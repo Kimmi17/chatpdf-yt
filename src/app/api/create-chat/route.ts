9c60014b1ca1590f1ad07c3000c5ec67f17ad0de
Kimmi17 <dangthikimthuybl@gmail.com>
2023-11-18 05:21:42 +0200

    Add database insertion and response handling

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -17,6 +17,23 @@ export async function POST(req: Request, res: Response) {
     const { file_key, file_name } = body;
     console.log(file_key, file_name);
     const pages = await loadS3IntoPinecone(file_key);
+    const chat_id = await db
+      .insert(chats)
+      .values({
+        fileKey: file_key,
+        pdfName: file_name,
+        pdfUrl: getS3Url(file_key),
+        userId,
+      })
+      .returning({
+        insertedId: chats.id,
+      });
+    return NextResponse.json(
+      {
+        chat_id: chat_id[0].insertedId,
+      },
+      { status: 200 }
+    );
   } catch (error) {
     console.error(error);
     return NextResponse.json(
