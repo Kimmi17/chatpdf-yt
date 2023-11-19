9bedf3bd5ef64a829c89e46f30a21e3f34fb30c5
Kimmi17 <dangthikimthuybl@gmail.com>
2023-11-19 23:28:41 +0200

    Create ChatPage component with user authentication

---
+++ src/app/chat/[chatId]/page.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+type Props = {
+  params: {
+    chatId: string;
+  };
+};
+
+const ChatPage = async ({ params: { chatId } }: Props) => {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+};
+
+export default ChatPage;
