bfb9bfcd4d1b51cd3f03a20356af03723b5bb1ac
Kimmi17 <dangthikimthuybl@gmail.com>
2023-11-20 16:14:56 +0200

    Stripe Integration

--- src/components/ChatSideBar.tsx
+++ src/components/ChatSideBar.tsx
@@ -6,13 +6,18 @@ import React from "react";
 import { Button } from "./ui/button";
 import { MessageCircle, PlusCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
+import axios from "axios";
+import SubscriptionButton from "./SubscriptionButton";
 
 type Props = {
   chats: DrizzleChat[];
   chatId: number;
+  isPro: boolean;
 };
 
-const ChatSideBar = ({ chats, chatId }: Props) => {
+const ChatSideBar = ({ chats, chatId, isPro }: Props) => {
+  const [loading, setLoading] = React.useState(false);
+
   return (
     <div className="w-full h-screen p-4 text-gray-200 bg-gray-900">
       <Link href="/">
@@ -22,7 +27,7 @@ const ChatSideBar = ({ chats, chatId }: Props) => {
         </Button>
       </Link>
 
-      <div className="flex max-h-screen overflow-scroll pb-20 flex-col gap-2 mt-4">
+      <div className="flex flex-col gap-2 mt-4">
         {chats.map((chat) => (
           <Link key={chat.id} href={`/chat/${chat.id}`}>
             <div
@@ -38,6 +43,13 @@ const ChatSideBar = ({ chats, chatId }: Props) => {
             </div>
           </Link>
         ))}
+      </div>
+      <div className="absolute bottom-4 left-4">
+        <div className="flex items-center gap-2 text-sm text-slate-500 flex-wrap">
+          <Link href="/">Home</Link>
+          <Link href="/">Source</Link>
+        </div>
+        <SubscriptionButton isPro={isPro} />
       </div>
     </div>
   );
