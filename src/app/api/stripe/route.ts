80e8041ea90f96ccc539454b021ae6fdf6002be5
Kimmi17 <dangthikimthuybl@gmail.com>
2023-11-20 15:11:35 +0200

    Stripe Integration

---
+++ src/app/api/stripe/route.ts
@@ -0,0 +1,31 @@
+import { db } from "@/lib/db";
+import { userSubscriptions } from "@/lib/db/schema";
+import { stripe } from "@/lib/stripe";
+import { auth, currentUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+const return_url = process.env.NEXT_BASE_URL + "/";
+
+export async function GET() {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+
+    if (!userId) {
+      return new NextResponse("unauthorized", { status: 401 });
+    }
+    const _userSubscriptions = await db
+      .select()
+      .from(userSubscriptions)
+      .where(eq(userSubscriptions.userId, userId));
+    if (_userSubscriptions[0] && _userSubscriptions[0].stripeCustomerId) {
+      // trying to cancel at the billing portal
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: _userSubscriptions[0].stripeCustomerId,
+        return_url,
+      });
+      return NextResponse.json({ url: stripeSession.url });
+    }
+  } catch (error) {}
+}
