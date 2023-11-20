bdb874dd52944d81f4cf06fa56178bbb48a28927
Kimmi17 <dangthikimthuybl@gmail.com>
2023-11-20 13:52:45 +0200

    Add getContext function

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,5 +1,6 @@
 import { Pinecone, PineconeClient } from "@pinecone-database/pinecone";
 import { convertToAscii } from "./utils";
+import { getEmbeddings } from "./embeddings";
 
 export async function getMatchesFromEmbeddings(
   embeddings: number[],
@@ -27,4 +28,20 @@ export async function getMatchesFromEmbeddings(
     console.log("error querying embeddings", error);
     throw error;
   }
+}
+
+export async function getContext(query: string, fileKey: string) {
+  const queryEmbeddings = await getEmbeddings(query);
+  const matches = await getMatchesFromEmbeddings(queryEmbeddings, fileKey);
+
+  const qualifyingDocs = matches.filter(
+    (match) => match.score && match.score > 0.7
+  );
+  type Metadata = {
+    text: string;
+    pageNumber: number;
+  };
+  let docs = qualifyingDocs.map((match) => (match.metadata as Metadata).text);
+  // 5 vectors
+  return docs.join("\n").substring(0, 3000);
 }
